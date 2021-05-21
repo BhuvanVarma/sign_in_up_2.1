e14463dfa721100489c541f230087cb3567487c5
BhuvanVarma <54587726+BhuvanVarma@users.noreply.github.com>
2021-05-21 18:22:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+function showPass(){
+	var x = document.getElementById('myInput');
+	var y = document.getElementById('show');
+	var z = document.getElementById('hide');
+
+	if (x.type === 'password') {
+		x.type = 'text';
+		y.style.display = 'block';
+		z.style.display = 'none';
+	}
+
+	else{
+		x.type = 'password';
+		y.style.display = 'none';
+		z.style.display = 'block';
+	}
+
+}
